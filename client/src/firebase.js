70100b9b727f3271f92a5c07443b3f78e0cb6d03
dhruvgarg4521 <dhruvgarg4521@gmail.com>
2024-09-05 02:38:33 +0530

    updated firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,15 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "publishhub-9fade.firebaseapp.com",
-  projectId: "publishhub-9fade",
-  storageBucket: "publishhub-9fade.appspot.com",
-  messagingSenderId: "872104594234",
-  appId: "1:872104594234:web:4da665bb28dd41f6e35e03",
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: "publishhub-e5f9a.firebaseapp.com",
+  projectId: "publishhub-e5f9a",
+  storageBucket: "publishhub-e5f9a.appspot.com",
+  messagingSenderId: "53267649716",
+  appId: "1:53267649716:web:65195f7dc704f6cc9033e1"
 };
 
 // Initialize Firebase
